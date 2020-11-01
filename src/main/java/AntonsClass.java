100820c41696093194eb3214cf0436a700d21241
git config --global user.name <yonashaile12@gmail.com>
2020-11-01 14:36:57 -0600

    updated anton class

--- src/main/java/AntonsClass.java
+++ src/main/java/AntonsClass.java
@@ -1,5 +1,9 @@
 public class AntonsClass {
     public static void main(String[] args) {
         System.out.println("new file");
+
+        /** just experimenting
+        // no you should have some instance variables
+         */
     }
 }
