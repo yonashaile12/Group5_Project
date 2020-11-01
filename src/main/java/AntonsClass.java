f8924bf836d13b04e7396d39e161627d0ae7760f
AChernetskyy <directvusa2010@gmail.com>
2020-11-01 15:06:53 -0600

    Second commit on Antons branch

--- src/main/java/AntonsClass.java
+++ src/main/java/AntonsClass.java
@@ -2,5 +2,6 @@ public class AntonsClass {
     public static void main(String[] args) {
         System.out.println("new file");
         //new update in Antons branch
+        //second update on antons branch
     }
 }
